d9a728679bf3055e5fae51e320d2877de29c1485
sengan-s <sengansolaiappan@gmail.com>
2026-06-27 19:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (78.35%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,5 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        
+   
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
@@ -11,3 +13,5 @@ class Solution {
         
     }
 }
+        
+    
