29710c02b4e157807a128f10ed21f7d2df520686
sengan-s <sengansolaiappan@gmail.com>
2026-07-15 21:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (97.69%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans=new StringBuilder();
+        while(columnNumber>0){
+
+            columnNumber--;
+            int res=columnNumber%26;
+            ans.append((char)('A'+ res));
+            columnNumber=columnNumber/26;
+
+        }
+        return ans.reverse().toString();
+
+        
+    }
+}
