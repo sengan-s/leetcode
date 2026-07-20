d18c0ce6e48ec058401ee62251d0a59e2b516807
sengan-s <sengansengan96@gmail.com>
2026-07-20 19:13:56 +0530

    Time: 1 ms (99.98%) | Memory: 42.6 MB (63.93%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int sum=0;
+        while(x!=0){
+            int dig=x%10;
+            x=x/10;
+            if((sum>Integer.MAX_VALUE/10) ||(sum==Integer.MAX_VALUE/10 && dig>7)){
+                return 0;
+            }
+            if((sum<Integer.MIN_VALUE/10 )||(sum==Integer.MIN_VALUE/10 && dig<(-8))){
+                return 0;
+            }
+            
+                sum=sum*10+dig;
+            
+            
+        }
+        return sum;
+    }
+}
