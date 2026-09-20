a4e7a625eb106f61de70d1ce550b7e09bc696c55
sengan-s <sengansengan96@gmail.com>
2026-09-20 14:16:30 +0530

    Time: 4 ms (92.34%) | Memory: 82.9 MB (47.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,28 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int low = 1;
-        int high = nums.length - 1; // Since array length is n + 1, max value is n
+        // Step 1: Find the intersection point of the two runners
+        int slow = nums[0];
+        int fast = nums[0];
         
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            int count = 0;
-            
-            // Count how many numbers in the array are less than or equal to 'mid'
-            for (int num : nums) {
-                if (num <= mid) {
-                    count++;
-                }
-            }
-            
-            // Pigeonhole Principle: if count > mid, the duplicate is in the lower half
-            if (count > mid) {
-                high = mid;
-            } else {
-                // Otherwise, the duplicate is in the upper half
-                low = mid + 1;
-            }
+        do {
+            slow = nums[slow];           // moves 1 step
+            fast = nums[nums[fast]];     // moves 2 steps
+        } while (slow != fast);
+        
+        // Step 2: Find the entrance to the cycle (the duplicate number)
+        slow = nums[0]; // reset slow to the start
+        
+        while (slow != fast) {
+            slow = nums[slow];           // both move 1 step
+            fast = nums[fast];
         }
         
-        return low;
+        return slow; // or fast, they are the same at the entrance
     }
 }
